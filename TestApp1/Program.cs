dce6a9b4df1f3c320e6f27d44dfbdf4f1aeaa0a9
Sephydev <nicolas.bussiere@outlook.fr>
2026-03-25 21:07:02 +0100

    Now we have many messages

--- TestApp1/Program.cs
+++ TestApp1/Program.cs
@@ -1,1 +1,10 @@
 ﻿Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
+Console.WriteLine("Hello, C# Academy! One more modification!");
